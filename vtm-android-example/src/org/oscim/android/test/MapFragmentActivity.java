dd9727f3e7fc78778ae05ab118d8ecc5c4a4b137
Mathieu De Brito <mathieu.debrito@gmail.com>
2016-11-09 11:09:16 +0100

    Refactored to make a simple fragment example

--- vtm-android-example/src/org/oscim/android/test/MapFragmentActivity.java
+++ vtm-android-example/src/org/oscim/android/test/MapFragmentActivity.java
@@ -5,8 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -23,79 +21,36 @@ import org.oscim.theme.VtmThemes;
 import org.oscim.tiling.TileSource;
 import org.oscim.tiling.source.oscimap4.OSciMap4TileSource;
 
-public class MultiMapFragmentActivity extends FragmentActivity {
+public class MapFragmentActivity extends FragmentActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Tile.SIZE = Tile.calculateTileSize(getResources().getDisplayMetrics().scaledDensity);
-        setContentView(R.layout.activity_map_multi_fragment);
+        setContentView(R.layout.activity_map_fragment);
 
         setTitle(getClass().getSimpleName());
 
-        addFragment();
-    }
-
-    public void addFragment() {
         MapFragment newFragment = new MapFragment();
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container, newFragment)
-                .addToBackStack(newFragment.getName())
                 .commit();
     }
 
-    @Override
-    public void onBackPressed() {
-        int nbMapFragments = getSupportFragmentManager().getBackStackEntryCount();
-        if (nbMapFragments > 1) {
-            getSupportFragmentManager().popBackStackImmediate();
-        } else {
-            finish();
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.itmAddFragment:
-                addFragment();
-                return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_multi_map_fragments_menu, menu);
-        return true;
-    }
-
     public static class MapFragment extends Fragment {
-
-        private static int nbFragments = 0;
 
         private MapView mMapView;
         private Map mMap;
         private TileSource mTileSource;
         private MapPreferences mPrefs;
         private VectorTileLayer mBaseLayer;
-        private int currentFragmentNumber;
-
-        public MapFragment() {
-            super();
-            currentFragmentNumber = nbFragments;
-            nbFragments++;
-        }
 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View contentView = super.onCreateView(inflater, container, savedInstanceState);
             if (contentView == null) {
-                contentView = inflater.inflate(R.layout.fragment_map_multi_fragment, container, false);
+                contentView = inflater.inflate(R.layout.fragment_map, container, false);
             }
             return contentView;
         }
@@ -105,8 +60,9 @@ public class MultiMapFragmentActivity extends FragmentActivity {
             super.onViewCreated(view, savedInstanceState);
 
             mMapView = (MapView) view.findViewById(R.id.mapView);
+            mMapView.setZOrderOnTop(true);
             mMap = mMapView.map();
-            mPrefs = new MapPreferences(getName(), getContext());
+            mPrefs = new MapPreferences(MapFragment.class.getName(), getContext());
 
             mTileSource = new OSciMap4TileSource();
             mBaseLayer = mMap.setBaseMap(mTileSource);
@@ -138,10 +94,6 @@ public class MultiMapFragmentActivity extends FragmentActivity {
 
             mMapView.onPause();
             mPrefs.save(mMapView.map());
-        }
-
-        public String getName() {
-            return MapFragment.class.getName() + currentFragmentNumber;
         }
     }
 }
