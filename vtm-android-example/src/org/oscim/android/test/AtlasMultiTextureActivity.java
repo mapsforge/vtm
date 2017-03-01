ab8593688cfcfb0d86506f2a7b4f8e2e1b32233f
Emux <devemux86@gmail.com>
2017-03-01 21:00:56 +0200

    AtlasMultiTextureActivity: fix inheritance #311

--- vtm-android-example/src/org/oscim/android/test/AtlasMultiTextureActivity.java
+++ vtm-android-example/src/org/oscim/android/test/AtlasMultiTextureActivity.java
@@ -19,7 +19,6 @@
 package org.oscim.android.test;
 
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.widget.Toast;
 
 import org.oscim.backend.CanvasAdapter;
@@ -48,8 +47,7 @@ public class AtlasMultiTextureActivity extends MarkerOverlayActivity {
     private java.util.Map<Object, TextureRegion> regionsMap;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    void createLayers() {
         mBitmapLayer.tileRenderer().setBitmapAlpha(0.5f);
 
         // Map events receiver
