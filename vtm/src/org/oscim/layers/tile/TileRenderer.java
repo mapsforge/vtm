de6714a6a657258b26dc3a396b62f688fe8e1c96
Gustl22 <user.rebo@gmx.de>
2018-03-05 14:18:06 +0100

    set tile state with proxy to NEW_DATA

--- vtm/src/org/oscim/layers/tile/TileRenderer.java
+++ vtm/src/org/oscim/layers/tile/TileRenderer.java
@@ -159,8 +159,7 @@ public abstract class TileRenderer extends LayerRenderer {
             /* load tile that is referenced by this holder */
             MapTile proxy = tile.holder;
             if (proxy != null && (proxy.state(NEW_DATA) || proxy.state(READY))) {
-                if (proxy.state(READY))
-                    tile.state = NEW_DATA; // Changing independently of tile state, as long as it isn't READY
+                tile.state = NEW_DATA; // Changing independently of tile state, as long as it isn't READY
                 //uploadCnt += uploadTileData(proxy); // Should already been done in separate call
                 uploadCnt += uploadTileData(tile); // Actual tile must be loaded immediately
                 continue;
