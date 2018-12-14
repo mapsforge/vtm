d0a0cf9984e7e52a6824baeec7fe45a0006e9602
Gustl22 <user.rebo@gmx.de>
2018-12-14 14:03:55 +0100

    Reorder method / field modifiers

--- vtm-web/src/org/oscim/gdx/emu/com/badlogic/gdx/backends/gwt/GwtApplication.java
+++ vtm-web/src/org/oscim/gdx/emu/com/badlogic/gdx/backends/gwt/GwtApplication.java
@@ -572,7 +572,7 @@ public abstract class GwtApplication implements EntryPoint, Application {
         }
     }
 
-    native static public void consoleLog(String message) /*-{
+    static native public void consoleLog(String message) /*-{
         console.log( "GWT: " + message );
     }-*/;
 
