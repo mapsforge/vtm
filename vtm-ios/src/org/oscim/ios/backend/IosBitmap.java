40c0f7796c9e3d61a91b84eb4164c00fe627f431
Longri <develop@longri.de>
2019-07-29 18:53:15 +0200

    update RoboVm to version 2.3.8, depended on the update of LibGdx to version 1.9.10 fix CgBitmapContext changes on RoboVm

--- vtm-ios/src/org/oscim/ios/backend/IosBitmap.java
+++ vtm-ios/src/org/oscim/ios/backend/IosBitmap.java
@@ -32,6 +32,7 @@ import org.robovm.apple.coregraphics.CGRect;
 import org.robovm.apple.foundation.NSData;
 import org.robovm.apple.uikit.UIColor;
 import org.robovm.apple.uikit.UIImage;
+import org.robovm.rt.VM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +40,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.Buffer;
+import java.nio.ByteOrder;
 
 /**
  * iOS specific implementation of {@link Bitmap}.
@@ -217,7 +219,8 @@ public class IosBitmap implements Bitmap {
             glFormat = pixmap.getGLFormat();
             glType = pixmap.getGLType();
 
-            directPixelBuffer = cgBitmapContext.getData().asIntBuffer(encodedData.length / 4);
+            directPixelBuffer = VM.newDirectByteBuffer(cgBitmapContext.getData(), (encodedData.length / 4) << 2)
+                    .order(ByteOrder.nativeOrder()).asIntBuffer();
             pixmap.dispose();
 
         }
